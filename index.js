e42cfd1b8b0eb1e2c29c307d8fc95f56ff634dfe
Michael Morran <michaelmorran@Michaels-MacBook-Pro-6.local>
2021-02-22 20:33:58 -0600

    login protocol added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let availColors = ['red','blue','green','white','pink','orange','yellow'];
+let availInstruments = ["I1", "I2", "I3", "I4", "I5", "I6", "I7"];
+
 let express = require('express');
 let app = express();
 app.use('/', express.static('public'));
@@ -13,9 +16,43 @@ let io = require('socket.io')(server);
 
 // Listen for Individual Connection
 io.sockets.on('connection', function(socket) {
-    console.log ('New client: ' + socket.id);
+    console.log('New client: ' + socket.id);
+    let socketObject = [socket.id];
+    // console.log(serverObject);
+    socket.on("beginLogin", (beginLogin) => {
+        console.log(beginLogin);
+        let serverObject = {
+            "availableColors" : availColors,
+            "availableInstruments" : availInstruments,
+        };
+        io.emit('login', serverObject);
+    });
+    
+    socket.on('colorPicked', (userObject) => {
+    //    console.log(userObject);
+       socketObject.push(userObject);
+    //    console.log(socketObject);
+       for (i = 0; i < availColors.length; i++) {
+           if (userObject.color == availColors[i]) {
+               availColors.splice(i, 1);
+            //    console.log(availColors);
+           }
+           if (userObject.instrument == availInstruments[i]) {
+            availInstruments.splice(i, 1);
+            // console.log(availInstruments);
+        }
+       }
+    });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected: ' + socket.id);
+        try {
+        availInstruments.push(socketObject[1].instrument);
+        // console.log(availInstruments);
+        availColors.push(socketObject[1].color);
+        // console.log(availColors);
+        } catch {
+            console.log('no object available');
+        }
     });
 });
--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+var socket = io(); 
+
 console.log('hello world');
 let canvas;
 let windowWidth = 0;
@@ -5,9 +7,45 @@ let windowHeight = 0;
 let colors = ['red','blue','green','white','pink','orange','yellow'];
 let circleSize = 100;
 let mySound;
+let loginRunning = true;
+let availableInstruments;
+let availableColors;
+let userObject;
+
+
+function loginProtocol() {
+  let beginLogin = true;
+  socket.emit("beginLogin", beginLogin);
+  socket.on('login', (serverObject) => {
+    console.log(serverObject);
+    availableInstruments = serverObject.availableInstruments;
+    console.log(availableInstruments);
+    availableColors = serverObject.availableColors;
+    console.log(availableColors);
+    for (i = 0; i < availableColors.length; i++) {
+      let colorButton = document.createElement('button');
+      colorButton.id = availableColors[i];
+      colorButton.innerHTML = availableColors[i];
+      colorButton.style.border = "none";
+      colorButton.style.borderRadius = "100%";
+      colorButton.style.backgroundColor = availableColors[i];
+      document.getElementById('buttonDisplay').appendChild(colorButton);
+      colorButton.addEventListener('click', ()=> {
+        console.log("clicked on button: " + colorButton.id);
+        let instrument = random(availableInstruments);
+        userObject = {
+          "color" : colorButton.id,
+          "instrument" : instrument
+        };
+        socket.emit('colorPicked', userObject);
+        document.getElementById('loginDiv').style.display = "none";
+        loginRunning = false;
+      });
+    }
+  });
+}
 
 function preload(){
-
   mySound = loadSound('clap.wav')
 }
 
@@ -19,19 +57,28 @@ function setup() {
   canvas.parent('CanvasDiv');
   background(0);
   setupGrid();
+
+  loginProtocol();
 }
 
 function setupGrid() {
   strokeWeight(3);
-  line(windowWidth*0.1, windowHeight*0.1, windowWidth*0.1, windowHeight*0.9);
-  line(windowWidth*0.1, windowHeight*0.9, windowWidth*0.9, windowHeight*0.9);
-  text('Volume', windowWidth/3, windowHeight*0.95, 2*windowWidth/3, windowHeight);
-  for (let i = 0; i < 10; i++) {
-    stroke(255, 100);
+  stroke(255, 255);
+  line(windowWidth*0.1, 0, windowWidth*0.1, windowHeight*0.9);
+  line(windowWidth*0.1, windowHeight*0.9, windowWidth, windowHeight*0.9);
+  fill(255);
+  push();
+  textAlign(CENTER);
+  noStroke();
+  textSize(22);
+  text('Volume', windowWidth/2, windowHeight*0.95);
+  text('Pitch', windowWidth*0.05, windowHeight/2);
+  for (let i = 1; i < 10; i++) {
+    stroke(255, 255);
     line(i*windowWidth/10, windowHeight*0.92, i*windowWidth/10, windowHeight*0.88);
     line(windowWidth*0.08, i*windowHeight/10, windowWidth*0.12, i*windowHeight/10);
   }
-  for (let i = 0; i < 10; i++) {
+  for (let i = 1; i < 10; i++) {
     stroke(255, 60);
     line(i*windowWidth/10, windowHeight*0.9, i*windowWidth/10, 0);
     line(windowWidth*0.1, i*windowHeight/10, windowWidth, i*windowHeight/10);
@@ -39,12 +86,14 @@ function setupGrid() {
 }
 
 function mouseClicked(){
-  console.log('mouseclicked');
-//   fill(255);
-  fill(colors[0]);
-  ellipse(mouseX,mouseY,circleSize,circleSize);
-  mySound.play();
-  
+  if (!loginRunning) {
+    console.log('mouseclicked');
+    //   fill(255);
+      fill(colors[0]);
+      ellipse(mouseX,mouseY,circleSize,circleSize);
+      mySound.play();
+      
+  }
 }
 
 function windowResized() {
